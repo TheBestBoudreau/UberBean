b0e8ab821a5f22e39e27a99a5d7b719c2f15e848
Tyler Boudreau <boudreaut@hotmail.com>
2018-04-27 14:17:53 -0700

    added to new branch

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Tyler Boudreau on 2018-04-27.
+//  Copyright © 2018 Tyler Boudreau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Tyler Boudreau on 2018-04-27.
+//  Copyright © 2018 Tyler Boudreau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
